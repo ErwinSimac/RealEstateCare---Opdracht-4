f73140d2ec6275dad4e3fd3b1a61d40f628cd66c
Erwin de Rooij <erwin.de.rooij@simac.com>
2025-11-05 14:21:12 +0100

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,18 @@ import Logo from './assets/Logo REC.jpg'
 
 function App() {
   return (
-    <div style={{ textAlign: 'center', minHeight: '100vh', position: 'relative' }}>
+    <div style={{ minHeight: '100vh' }}>
       
-      {/* Bovenste kleurblok */}
-      <div style={{
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        width: '100%',
-        height: '10vh',              // 10% van de viewport hoogte
-        backgroundColor: 'rgba(0,170,162,0.3)'  // alfa 0.3 = 30% zichtbaar
-      }}></div>
-
-      {/* Content */}
-      <div style={{ paddingTop: '50px', position: 'relative', zIndex: 1 }}>
+      {/* Bovenste kleurblok (10% hoogte) */}
+      <div style={{ height: '10vh', backgroundColor: 'rgba(0,170,162,0.3)' }}></div>
+      
+      {/* Content onder het kleurblok */}
+      <div style={{ textAlign: 'center', paddingTop: '20px' }}>
         <h1 style={{ color: '#004080', marginBottom: '20px' }}>Hello World</h1>
         <img src={Logo} alt="RealEstateCare Logo" style={{ width: '200px', marginBottom: '20px' }} />
         <p>Welkom bij de RealEstateCare applicatie</p>
       </div>
-
+      
     </div>
   )
 }
