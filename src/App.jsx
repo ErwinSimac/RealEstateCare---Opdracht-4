63aee00df6023c20c92e3d29830cafe6dbbeb9ed
Erwin de Rooij <erwin.de.rooij@simac.com>
2025-11-05 14:26:19 +0100

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,14 @@ function App() {
   return (
     <div className="App">
       <div className="TopBlock"></div>
+
       <div className="AppContent">
         <h1 style={{ color: '#004080', marginBottom: '20px' }}>Hello World</h1>
         <img src={Logo} alt="RealEstateCare Logo" style={{ width: '200px', marginBottom: '20px' }} />
         <p>Welkom bij de RealEstateCare applicatie</p>
       </div>
+
+      <div className="BottomBlock"></div>
     </div>
   );
 }
