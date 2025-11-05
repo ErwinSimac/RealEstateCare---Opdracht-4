fefe02f6a6fce76c17967bce38af22c38289bb1d
Erwin de Rooij <erwin.de.rooij@simac.com>
2025-11-05 14:24:00 +0100

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import './App.css'
-import Logo from './assets/Logo REC.jpg'
+import './App.css';
+import Logo from './assets/Logo REC.jpg';
 
 function App() {
   return (
     <div className="App">
       <div className="TopBlock"></div>
       <div className="AppContent">
-        <h1>Hello World</h1>
+        <h1 style={{ color: '#004080', marginBottom: '20px' }}>Hello World</h1>
         <img src={Logo} alt="RealEstateCare Logo" style={{ width: '200px', marginBottom: '20px' }} />
         <p>Welkom bij de RealEstateCare applicatie</p>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
